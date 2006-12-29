74b015822a3f783e88ef0d9fc6db368ddd3f1e7f
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2006-12-29 00:06:39 +0000

    add hook for tt_news: Marker ###COMMENTCOUNT### for newstemplate added
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sk_pagecomments/trunk@4436 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,4 +8,11 @@ t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
 
 
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_skpagecomments_pi1.php','_pi1','list_type',0);
+
+#Adds a hook for tt_news to handle own markers
+if (TYPO3_MODE!='BE')	{
+    require_once(t3lib_extMgm::extPath('sk_pagecomments').'hooks/class.skpagecomments_tt_news_markers.php');
+}
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tt_news']['extraItemMarkerHook'][]='tx_skpagecomments_tt_news_markers';
+
 ?>
