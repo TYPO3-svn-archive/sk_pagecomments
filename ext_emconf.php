e8d5ae3c18266cd0940fc737ca3c7dbaf6d90aca
steffenk <steffenk@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-13 19:03:56 +0000

    bugfixes / cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sk_pagecomments/trunk@24333 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sk_pagecomments"
 #
-# Auto generated 20-11-2007 18:27
+# Auto generated 13-09-2009 01:25
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -41,8 +41,6 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'_md5_values_when_last_written' => 'a:53:{s:9:"ChangeLog";s:4:"66bb";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"17b8";s:17:"ext_localconf.php";s:4:"ad3f";s:14:"ext_tables.php";s:4:"f782";s:14:"ext_tables.sql";s:4:"2387";s:24:"ext_typoscript_setup.txt";s:4:"ef6a";s:35:"icon_tx_skpagecomments_comments.gif";s:4:"475a";s:39:"icon_tx_skpagecomments_pagecomments.gif";s:4:"475a";s:16:"locallang_db.php";s:4:"fcfb";s:16:"locallang_db.xml";s:4:"dc5d";s:7:"tca.php";s:4:"97ee";s:14:"doc/manual.sxw";s:4:"2748";s:19:"doc/wizard_form.dat";s:4:"7385";s:20:"doc/wizard_form.html";s:4:"cfd2";s:45:"hooks/class.tx_skpagecomments_markerhooks.php";s:4:"3350";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"4b4e";s:14:"mod1/index.php";s:4:"18fb";s:18:"mod1/locallang.xml";s:4:"94de";s:22:"mod1/locallang_mod.xml";s:4:"dbd4";s:19:"mod1/moduleicon.gif";s:4:"17b8";s:35:"pi1/class.tx_skpagecomments_pi1.php";s:4:"9665";s:17:"pi1/locallang.xml";s:4:"576e";s:14:"res/answer.png";s:4:"ed46";s:37:"res/class.tx_skpagecomments_label.php";s:4:"3f17";s:13:"res/email.png";s:4:"af58";s:12:"res/hide.gif";s:4:"d324";s:13:"res/house.png";s:4:"99be";s:12:"res/link.png";s:4:"a5dc";s:15:"res/realurl.txt";s:4:"c513";s:17:"res/template.html";s:4:"61df";s:14:"res/unhide.gif";s:4:"89d9";s:21:"res/smileys/green.gif";s:4:"4fd8";s:28:"res/smileys/icon_biggrin.gif";s:4:"f970";s:29:"res/smileys/icon_confused.gif";s:4:"4aff";s:25:"res/smileys/icon_cool.gif";s:4:"25c8";s:24:"res/smileys/icon_cry.gif";s:4:"7605";s:24:"res/smileys/icon_eek.gif";s:4:"52e4";s:25:"res/smileys/icon_evil.gif";s:4:"1782";s:26:"res/smileys/icon_frown.gif";s:4:"5a50";s:24:"res/smileys/icon_mad.gif";s:4:"e435";s:28:"res/smileys/icon_mrgreen.gif";s:4:"54e8";s:28:"res/smileys/icon_neutral.gif";s:4:"4e8b";s:25:"res/smileys/icon_razz.gif";s:4:"7aec";s:28:"res/smileys/icon_redface.gif";s:4:"d7e9";s:29:"res/smileys/icon_rolleyes.gif";s:4:"1907";s:24:"res/smileys/icon_sad.gif";s:4:"5a50";s:26:"res/smileys/icon_smile.gif";s:4:"9ee6";s:30:"res/smileys/icon_surprised.gif";s:4:"ae73";s:25:"res/smileys/icon_wink.gif";s:4:"f058";s:21:"res/smileys/smile.gif";s:4:"0d33";s:23:"res/smileys/zwinker.gif";s:4:"eb2f";}',
-	'suggests' => array(
-	),
 );
 
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -10,7 +10,7 @@ t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_skpagecomments_pi1.php','_pi1','list_type',0);
 
 #Adds a hook for tt_news to handle own markers
-if (TYPO3_MODE!='BE')	{
+if (TYPO3_MODE == 'FE')	{
     require_once(t3lib_extMgm::extPath('sk_pagecomments').'hooks/class.tx_skpagecomments_markerhooks.php');
 }
 
